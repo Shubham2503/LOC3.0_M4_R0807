1ec245895ce352a9dadbbc5beb79c6ab981783c1
Shubham <Shubhamp7188@gmail.com>
2021-03-20 19:51:55 +0530

    added tags

--- client/src/pages/Createpost/index.js
+++ client/src/pages/Createpost/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import styles from './index.module.css'
-import { Form, Button, Col, Row } from 'react-bootstrap'
+import { Form, Button, Col } from 'react-bootstrap'
 import axios from 'axios'
 
 
@@ -8,6 +8,8 @@ const Createpost = () => {
     const [title, setTitle] = useState('')
     const [desc, setDesc] = useState('')
     const [url, setUrl] = useState('')
+    const [tag1, setTag1] = useState('')
+    const [tag2, setTag2] = useState('')
     const [selectedFile, setSelectedFile] = useState(null);
 
     const handleClick = async () => {
@@ -15,7 +17,11 @@ const Createpost = () => {
         await axios.post('/post/create', {
             title,
             description: desc,
-            images: url
+            images: url,
+            tags: [
+                {"tag" : tag1},
+                {"tag" : tag2}
+            ]
         })
         .then(res => {
             
@@ -38,6 +44,17 @@ const Createpost = () => {
                     <Form.Label>Description</Form.Label>
                     <Form.Control as="textarea" placeholder="Add Description" onChange={e => setDesc(e.target.value)}/>
                 </Form.Group>
+
+                <Form.Row>
+                    <Form.Group as={Col}>
+                        <Form.Label>Tag 1</Form.Label>
+                        <Form.Control type="text" name="tag1" placeholder="Tag 1" onChange={e => setTag1(e.target.value)}/>
+                    </Form.Group>
+                    <Form.Group as={Col}>
+                        <Form.Label>Tag 2</Form.Label>
+                        <Form.Control type="text" name="tag2" placeholder="Tag 2" onChange={e => setTag2(e.target.value)}/>
+                    </Form.Group>
+                </Form.Row>
 
                 <Form.Group>
                     <Form.Label>Image</Form.Label>
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import {Card, Button} from 'react-bootstrap'
+import {Card, Button, Badge, Col, Row, Container} from 'react-bootstrap'
 import styles from './index.module.css'
 
 const Home = () => {
-    const [data, setData] = useState([])
+    const [data, setData] = useState(null)
 
     useEffect(() => {
         getData()
@@ -28,7 +28,8 @@ const Home = () => {
             console.log(err)
         })
     }
-
+    if(data === null)
+    return null
     return (
         <>
             {data.map((val, ind) => {
@@ -37,9 +38,21 @@ const Home = () => {
                             <Card style={{ width: '18rem' }}>
                             <Card.Img height={180} width={100} variant="top" src={val.images} />
                             <Card.Body>
-                                <Card.Title>{val.title} Likes: {val.likes}</Card.Title>
+                                <Card.Title>{val.title}</Card.Title>
+                                {(val.tags.length > 0) && (
+                                    <>
+                                        <Badge pill variant="primary">
+                                            {val.tags[0].tag}
+                                        </Badge>{' '}
+                                        <Badge pill variant="success">
+                                            {val.tags[1].tag}
+                                        </Badge>
+                                        <br/>
+                                    </>
+                                )}
                                 <Card.Text>{val.description}</Card.Text>
-                                <Button variant="primary" onClick={(e) => handleClick(val._id)}>Like</Button>
+                                <Button variant="primary" onClick={(e) => handleClick(val._id)}>{val.likes} Likes</Button>
+                                
                             </Card.Body>
                             </Card>
                         </div>
