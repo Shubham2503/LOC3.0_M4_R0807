5a8e986bfb30460c20056069de7a727e24bd14bf
Shubham <Shubhamp7188@gmail.com>
2021-03-20 15:35:41 +0530

    Update index.js

--- client/src/pages/Exercise/index.js
+++ client/src/pages/Exercise/index.js
@@ -1,17 +1,32 @@
 import React, { useState } from 'react'
 import styles from './index.module.css'
 import { Form, Row, Col } from 'react-bootstrap'
+import axios from 'axios'
+
 
 const Exercise = () => {
-    const [search,setSearch] = useState('');
+    const [search,setSearch] = useState('')
+    const [data,setData] = useState([])
     console.log(search)
+
+    const handleUpdate = async (e) => {
+        setSearch(e.target.value)
+        const url = 'https://trackapi.nutritionix.com/v2/search/instant?' + search
+        await axios.post(url,)
+        .then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
+        })
+    } 
+
     return (
         <div className={styles.container}>
             <h2>Current Score : </h2>
             <Form>
                 <Row>
                     <Col>
-                        <Form.Control placeholder="Enter FOOD" onChange={(e) => {setSearch(e.target.value)}}/>
+                        <Form.Control placeholder="Enter FOOD" onChange={handleUpdate}/>
                     </Col>
                 </Row>
             </Form>
