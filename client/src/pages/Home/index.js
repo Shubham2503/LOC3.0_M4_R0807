4ae85d66bb8c3296159b2ccdcf8e7d8b24152b29
Shubham <shubham>
2021-03-20 12:07:11 +0530

    added pages

---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <>
+            <h1>HOme</h1>
+        </>
+    )
+}
+
+export default Home
---
+++ client/src/pages/Scoreboard/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Scoreboard = () => {
+    return (
+        <>
+            <h1>SCoreboard</h1>
+        </>
+    )
+}
+
+export default Scoreboard
