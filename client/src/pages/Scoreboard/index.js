df133f4188fb17e6db0841d846f6227ff7b27f2d
Shubham <Shubhamp7188@gmail.com>
2021-03-20 12:57:05 +0530

    updated scoreboard

--- client/src/pages/Scoreboard/index.js
+++ client/src/pages/Scoreboard/index.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import styles from './index.module.css'
+import axios from 'axios'
+import Table from 'react-bootstrap/Table'
 
 const Scoreboard = () => {
+    const [transactions, setTransactions] = useState([])
+
+    useEffect(() => {
+        getTransaction()
+    }, [])
+
+
+    const getTransaction = async () => {
+        // await axios.get('/transaction')
+        //     .then(res => {
+        //         setTransactions(res.data.data)
+        //     }).catch(err => {
+        //         console.log(err)
+        //     })
+    }
+
     return (
-        <>
-            <h1>Scoreboard</h1>
-        </>
+        <div className={styles.container}>
+            <Table responsive striped bordered hover variant="dark">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>UserName</th>
+                        <th>Score</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {transactions.map((ele, index) => {
+                        return (
+                            <tr>
+                                <td key={index}>{index+1}</td>
+                                <td key={index}>{ele.user}</td>
+                                <td key={index}>{ele.score}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </Table>
+        </div>
     )
 }
 
