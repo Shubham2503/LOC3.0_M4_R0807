cf88ed424ab226ed75fb7715c28ef57620aeac28
Sahil-Shende <47074843+Sahil-Shende@users.noreply.github.com>
2021-03-20 12:56:29 +0530

    Update index.js

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -10,9 +10,28 @@ mongoose.connect(
     }
 );
 
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+ 
+
+
 const seedDB = async () => {
     await User.deleteMany({});
-    
+    for(let i = 0; i < 20; i++)
+    {
+        const user = new User({
+            username: makeid(5),
+            score: Math.floor((Math.random() * 100) + 1);
+        })
+        await user.save();
+    }
 }
 
 seedDB().then(() => {
