cf81dd259fba1fb631e89c1ba45c7ca8d4a1dd0f
Shubham <Shubhamp7188@gmail.com>
2021-03-21 04:21:00 +0530

    Update index.js

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,45 +1,44 @@
 import React from 'react'
 import styles from './index.module.css'
-
-
-
-
+import { Form, Button, Col, Modal, Table } from 'react-bootstrap'
 
 
 const Register = () => {
     return (
         <div className={styles.box}>
+            <h3>Register</h3>
+            <Form className="container">
+                <Form.Row>
+                    <Form.Group as={Col}>
+                        <Form.Label>User Name</Form.Label>
+                        <Form.Control type="text" name="title" placeholder="User Name" />
+                    </Form.Group>
+                </Form.Row>
+                <Form.Row>
+                    <Form.Group as={Col}>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" name="pass" placeholder="Password" />
+                    </Form.Group>
+                </Form.Row>
 
-            <form>
-
-                <h3>Register</h3>
-
-                <div className="form-group">
-                    <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
-                </div>
-
-                <div className="form-group">
-                    <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
-                </div>
-
-                <div className="form-group">
-                    <label>Email</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
-
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
-
-                <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
-                <p className="forgot-password text-right">
-                    Already registered <a href="#">log in?</a>
-                </p>
-
-            </form>
+                <Form.Row>
+                    <Form.Group as={Col}>
+                        <Form.Label>Height</Form.Label>
+                        <Form.Control type="number" name="height" placeholder="Height" />
+                    </Form.Group>
+                    <Form.Group as={Col}>
+                        <Form.Label>Weight</Form.Label>
+                        <Form.Control type="number" name="weight" placeholder="Weight" />
+                    </Form.Group>
+                    <Form.Group as={Col}>
+                        <Form.Label>Age</Form.Label>
+                        <Form.Control type="number" name="age" placeholder="Age" />
+                    </Form.Group>
+                </Form.Row>
+            </Form>
+            <Button variant="primary">
+                Register
+            </Button>
 
         </div>
     );
