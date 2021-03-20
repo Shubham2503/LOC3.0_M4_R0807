62f071744c2e645cc82b5921c2aafe7388f8f299
Ismail <56081151+IsmailZain@users.noreply.github.com>
2021-03-20 18:48:06 +0530

    Update index.js
    changed index.js

--- client/src/pages/Exercise/index.js
+++ client/src/pages/Exercise/index.js
@@ -1,47 +1,81 @@
 import React, { useState } from 'react'
 import styles from './index.module.css'
-import { Form, Row, Col } from 'react-bootstrap'
+import { Form, Row, Col ,Button} from 'react-bootstrap'
 import axios from 'axios'
 import Autocomplete from 'react-autocomplete'
 const Exercise = () => {
+    const [meal,setMeal] = useState("")
+    const [ex,setEx] = useState("")
+    const [disp,setDisp] = useState("")
+    const  [formdata, setFormdata] = useState("")
 
-    const [search,setSearch] = useState('')
-    const [data,setData] = useState([
+    const fetchMeal = async () => {
+
+        await axios.post(`https://trackapi.nutritionix.com/v2/natural/nutrients`, {
+            headers: {
+                "x-app-id": process.env.REACT_APP_API_ID,
+                "x-app-key": process.env.REACT_APP_API_KEY,
+                "x-remote-user-id": process.env.REACT_APP_REMOTE_ID
+            }
+        })
+            .then(res => {
+                const temp_data = res.data.common.map((val, ind) => {
+                   console.log(temp_data)
+                })
+                setData(temp_data)
+            }).catch(err => {
+                console.log(err)
+            })
+
+    }
+    const submit = (e) => {
+        console.log("worked")
+        fetchMeal();
+            
+    }
+    const ipHandler1 = (e) => {
+        setMeal(e.target.value)
+    }
+    const ipHandler2 = (e) => {
+        setEx(e.target.value)
+    }
+    const [search, setSearch] = useState('')
+    const [data, setData] = useState([
         { label: 'apple' },
         { label: 'banana' },
         { label: 'pear' }
     ])
     console.log(search)
-    console.log(process.env.REACT_APP_API_ID)
+   // console.log(process.env.REACT_APP_API_ID)
 
 
-    const handleUpdate = async (e,results) => {
+    const handleUpdate = async (e, results) => {
         setSearch(e.target.value)
 
 
-        await axios.get(`https://trackapi.nutritionix.com/v2/search/instant?query=${search}`,{
+        await axios.get(`https://trackapi.nutritionix.com/v2/search/instant?query=${search}`, {
             headers: {
-                "x-app-id" : process.env.REACT_APP_API_ID,
-                "x-app-key" : process.env.REACT_APP_API_KEY,
-                "x-remote-user-id" : process.env.REACT_APP_REMOTE_ID
+                "x-app-id": process.env.REACT_APP_API_ID,
+                "x-app-key": process.env.REACT_APP_API_KEY,
+                "x-remote-user-id": process.env.REACT_APP_REMOTE_ID
             }
         })
-        .then(res => {
-            const temp_data = res.data.common.map((val, ind) => {
-                return {
-                    label: val.food_name
-                }
+            .then(res => {
+                const temp_data = res.data.common.map((val, ind) => {
+                    return {
+                        label: val.food_name
+                    }
+                })
+                setData(temp_data)
+            }).catch(err => {
+                console.log(err)
             })
-            setData(temp_data)
-        }).catch(err => {
-            console.log(err)
-        })
-    } 
+    }
 
     return (
         <div className={styles.container}>
             <h2>Current Score : </h2>
-            <Form>
+            <Form >
                 <Form.Group as={Row} controlId="formHorizontalEmail">
                     {/* <Form.Label column sm={2}>
                     Food
@@ -61,7 +95,7 @@ const Exercise = () => {
                         onSelect={(val) => setSearch(val)}
                     />
                     </Col> */}
-                    
+
                     <Form.Label column sm={2}>
                         Exercise
                     </Form.Label>
@@ -69,7 +103,19 @@ const Exercise = () => {
                         <Form.Control type="textarea" placeholder="1000 steps" />
                     </Col>
                 </Form.Group>
+
+                <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>So What do you had for your Meal ?? </Form.Label>
+                    <Form.Control onChange = {ipHandler1} as="textarea" rows={3} onClick = {submit} />
+                </Form.Group>
+
+                <Form.Group controlId="exampleForm.ControlTextarea2">
+                    <Form.Label>Tell Me About Your workout session??</Form.Label>
+                    <Form.Control onChange = {ipHandler2} as="textarea" rows={3} />
+                </Form.Group>
+                <Button onClick = {submit}  variant="primary">Submit</Button>{' '}
             </Form>
+            
         </div>
     )
 }
