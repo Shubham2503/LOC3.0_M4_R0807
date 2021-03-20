de9f2cbaae130d037aa584d4a5d8aa0be819f7b6
Shubham <Shubhamp7188@gmail.com>
2021-03-20 17:19:17 +0530

    added search box

--- client/src/pages/Exercise/index.js
+++ client/src/pages/Exercise/index.js
@@ -2,19 +2,37 @@ import React, { useState } from 'react'
 import styles from './index.module.css'
 import { Form, Row, Col } from 'react-bootstrap'
 import axios from 'axios'
-
-
+import Autocomplete from 'react-autocomplete'
 const Exercise = () => {
-    const [search,setSearch] = useState('')
-    const [data,setData] = useState([])
-    console.log(search)
 
-    const handleUpdate = async (e) => {
+    const [search,setSearch] = useState('')
+    const [data,setData] = useState([
+        { label: 'apple' },
+        { label: 'banana' },
+        { label: 'pear' }
+    ])
+    console.log(search)
+    console.log(process.env.REACT_APP_API_ID)
+
+
+    const handleUpdate = async (e,results) => {
         setSearch(e.target.value)
-        const url = 'https://trackapi.nutritionix.com/v2/search/instant?' + search
-        await axios.post(url,)
+
+
+        await axios.get(`https://trackapi.nutritionix.com/v2/search/instant?query=${search}`,{
+            headers: {
+                "x-app-id" : process.env.REACT_APP_API_ID,
+                "x-app-key" : process.env.REACT_APP_API_KEY,
+                "x-remote-user-id" : process.env.REACT_APP_REMOTE_ID
+            }
+        })
         .then(res => {
-            console.log(res)
+            const temp_data = res.data.common.map((val, ind) => {
+                return {
+                    label: val.food_name
+                }
+            })
+            setData(temp_data)
         }).catch(err => {
             console.log(err)
         })
@@ -24,9 +42,21 @@ const Exercise = () => {
         <div className={styles.container}>
             <h2>Current Score : </h2>
             <Form>
-                <Row>
+                <Row className={styles.row}>
                     <Col>
-                        <Form.Control placeholder="Enter FOOD" onChange={handleUpdate}/>
+                    <Autocomplete
+                        className={styles.search}
+                        getItemValue={(item) => item.label}
+                        items={data}
+                        renderItem={(item, isHighlighted) =>
+                            <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+                            {item.label}
+                            </div>
+                        }
+                        value={search}
+                        onChange={handleUpdate}
+                        onSelect={(val) => setSearch(val)}
+                        />
                     </Col>
                 </Row>
             </Form>
