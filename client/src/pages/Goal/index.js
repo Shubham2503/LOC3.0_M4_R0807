54cff9c133e2fbc5186ce843e12235bb5b967833
Shubham <Shubhamp7188@gmail.com>
2021-03-21 01:26:59 +0530

    add goal func

--- client/src/pages/Goal/index.js
+++ client/src/pages/Goal/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import styles from './index.module.css'
-import { Form, Button, Col, Modal } from 'react-bootstrap'
+import { Form, Button, Col, Modal, Table } from 'react-bootstrap'
 import axios from 'axios'
 
 
@@ -15,24 +15,26 @@ const Goal = () => {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    const handleClick = () => {
-        // await axios.post('/post/create', {
-        //     title,
-        //     description: desc,
-        //     time
-        // })
-        // .then(res => {
-
-        // }).catch(err => {
-        //     console.log(err)
-        // })
+    const handleClick = async () => {
+        await axios.post('/goals/6055fd6eb17d53243c8015c1', {
+            title,
+            description: desc,
+            time
+        })
+        .then(res => {
+            getData()
+            handleClose()
+        }).catch(err => {
+            console.log(err)
+        })
     }
+
     useEffect(() => {
         getData()
     },[])
 
     const getData = async () => {
-        await axios.get('/goal/6055fd6eb17d53243c8015c1')
+        await axios.get('/goals/6055fd6eb17d53243c8015c1')
         .then(res => {
             setData(res.data)
         }).catch(err => {
@@ -81,6 +83,34 @@ const Goal = () => {
                     </Button>
                 </Modal.Footer>
             </Modal>
+
+            {(data !== null) && 
+                <Table className={styles.table} responsive striped bordered hover variant="dark">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Title</th>
+                            <th>description</th>
+                            <th>time</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((ele, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{index+1}</td>
+                                    <td>{ele.title}</td>
+                                    <td>{ele.description}</td>
+                                    <td>{ele.time}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </Table>
+            }
+
+
+
         </div>
     )
 }
