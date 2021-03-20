38b683fd0b937b3a359f043b7ca3b798b89e0d83
Shubham <Shubhamp7188@gmail.com>
2021-03-21 03:40:56 +0530

    added friend list

--- client/src/pages/User/index.js
+++ client/src/pages/User/index.js
@@ -1,26 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import styles from './index.module.css'
-import {Card, Button, Badge, Col, Row, Container} from 'react-bootstrap'
+import { Card, Button, Badge, Col, Row, Container, Form, Table } from 'react-bootstrap'
 import axios from 'axios'
 
 const User = () => {
 
     const [data, setData] = useState(null)
-
+    const [uname, setUname] = useState('')
     useEffect(() => {
         getData()
-    },[])
+    }, [])
 
     const getData = async () => {
         await axios.get('/user/6055fd6eb17d53243c8015c1')
+            .then(res => {
+                setData(res.data)
+            }).catch(err => {
+                console.log(err)
+            })
+    }
+
+
+    console.log(data)
+
+    const handleClick = async () => {
+        await axios.post(`/friend/6055fd6eb17d53243c8015c1/${uname}`)
         .then(res => {
-            setData(res.data)
+            getData()
         }).catch(err => {
             console.log(err)
         })
     }
-
-    console.log(data)
 
     if (data === null)
         return null
@@ -44,6 +54,43 @@ const User = () => {
                     <p>Age : {data.age}</p>
                     <p>Gender : {data.gender}</p>
                 </Col>
+            </Row>
+            <Row>
+                <Form>
+                    <Form.Row>
+                        <Form.Group as={Col}>
+                            <Form.Label>Add Friends</Form.Label>
+                            <Form.Control type="text" name="title" placeholder="Enter title" onChange={e => setUname(e.target.value)} />
+                        </Form.Group>
+                    </Form.Row>
+                    
+                    <Button variant="primary" onClick={handleClick}>
+                        Submit
+                    </Button>
+                </Form>
+            </Row>
+            <Row style={{width: '500px'}}>
+            {(data.friends !== null) && 
+                <Table className={styles.table} responsive striped bordered hover variant="dark">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Friends</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.friends.map((ele, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{index+1}</td>
+                                    <td>{ele.name}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </Table>
+            }
+                
             </Row>
         </Container>
     )
