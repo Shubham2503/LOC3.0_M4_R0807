7201042272329ba82c81287b342f3f15e4a26e97
Harsh Kumar <44499896+Harshkumar10@users.noreply.github.com>
2021-03-20 21:15:02 +0530

    adf

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,64 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-    username: String,
-    score: Number
-});
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            //emails to be unique
+            unique: true,
+            require: true,
+            trim: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 7,
+        },
+        age: {
+            type: Number,
+            default: 0,
+            required: true,
+            validate(value) {
+                if (value < 0) {
+                    throw new Error("Age must be a positive number");
+                }
+            },
+        },
+        weight: {
+            type: Number,
+            default: 0,
+            required: true,
+            validate(value) {
+                if (value < 0)
+                    throw new Error("weight must be greater than zero");
+            },
+        },
+        tokens: [
+            {
+                token: {
+                    type: String,
+                    require: true,
+                },
+            },
+        ],
 
-module.exports = mongoose.model('User', UserSchema);
+        score: {
+            type: Number,
+            default: 0,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model("User", UserSchema);
