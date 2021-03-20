e90d41b00fd89dc202cada4eaa075461cd49dc93
Ismail <56081151+IsmailZain@users.noreply.github.com>
2021-03-20 23:04:34 +0530

    done with user exercise and meal

--- client/src/pages/Exercise/index.js
+++ client/src/pages/Exercise/index.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react'
 import styles from './index.module.css'
 import { Form, Row, Col, Button, Toast } from 'react-bootstrap'
 import axios from 'axios'
-import Autocomplete from 'react-autocomplete'
+
 const Exercise = () => {
     const [meal, setMeal] = useState("")
     const [ex, setEx] = useState("")
     const [disp, setDisp] = useState("")
     const [calGained, setCalG] = useState(null)
     const [calLost, setCalL] = useState(null)
-    const [formdata, setFormdata] = useState("")
+    
+    const [score,setScore] = useState(0)
+
 
     const fetchMeal = async () => {
 
@@ -27,7 +29,7 @@ const Exercise = () => {
                 const temp_data = res.data.foods.map((val, ind) => {
                     return val.nf_calories
                 }).reduce((el, ac) => (el + ac))
-                console.log(temp_data)
+              //  console.log(temp_data)
                 setCalG(temp_data)
 
             }).catch(err => {
@@ -50,7 +52,7 @@ const Exercise = () => {
                 const temp_data = res.data.exercises.map((val, ind) => {
                     return val.nf_calories
                 }).reduce((el, ac) => (el + ac))
-                console.log(temp_data)
+              //  console.log(temp_data)
                 setCalL(temp_data)
             }).catch(err => {
                 console.log(err)
@@ -58,11 +60,16 @@ const Exercise = () => {
     }
 
 
-    const submit = async (e) => {
-        console.log("worked")
+    const calculate = async (e) => {
+        //console.log("worked")
         await fetchMeal();
         await fetchEx();
+        setScore((+calLost/+calGained).toFixed(2))
 
+    }
+    const submit = () => {
+        console.log("submitted")
+        // submitting to rest api by getting user's id
     }
     const ipHandler1 = (e) => {
         setMeal(e.target.value)
@@ -101,7 +108,7 @@ const Exercise = () => {
 
     return (
         <div className={styles.container}>
-            <h2>Current Score : </h2>
+            <h2>Current Score :  {score}</h2>
             <Form className="container" >
 
                 {/* <Form.Group as={Row} controlId="formHorizontalEmail">
@@ -136,40 +143,50 @@ const Exercise = () => {
                     <Form.Label  >So What do you had for your Meal ?? </Form.Label>
                     <Row>
                         <Col><Form.Control onChange={ipHandler1} as="textarea" rows={3} /></Col>
+
                     </Row>
+                    <Row>
+
+                        <Col>
+                            <h4>Calories Gained</h4>
+                            {calGained ? calGained : 0}
+
+                        </Col>
+                    </Row>
+
                 </Form.Group>
 
                 <Form.Group controlId="exampleForm.ControlTextarea2">
                     <Form.Label>Tell Me About Your workout session??</Form.Label>
                     <Row>
                         <Col><Form.Control onChange={ipHandler2} as="textarea" rows={3} /></Col>
-                    </Row>
-                </Form.Group>
 
-                <Form.Group controlId="exampleForm.ControlTextarea3">
+                    </Row>
                     <Row>
-                        <Col>
-                            <h4>Calories Gained</h4>
-                            {calGained ? calGained : 0}
-                        </Col>
+
                         <Col>
                             <h4>Calories Burned</h4>
                             {calLost ? calLost : 0}
+
                         </Col>
                     </Row>
 
                 </Form.Group>
-                <Button onClick={submit} variant="primary">Submit</Button>{' '}
+                <Button onClick={calculate} variant="primary">Calculate</Button>{' '}
+                <Button onClick={submit} variant="success">Submit</Button>{' '}
 
             </Form>
-            <Toast>
+            <div className = {styles.toast}>
+            <Toast >
                 <Toast.Header>
                     <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
-                    <strong className="mr-auto">Bootstrap</strong>
-                    <small>11 mins ago</small>
+                    <strong className="mr-auto">Total Calories Burned !!</strong>
+                    
                 </Toast.Header>
-                <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+                <Toast.Body>You haved burned {calLost ? calLost : 0} Calories</Toast.Body>
             </Toast>
+            </div>
+           
 
 
         </div>
