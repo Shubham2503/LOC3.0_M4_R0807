b90e7af93245f79ec4e3dbd78c92f3672822248f
Ismail <56081151+IsmailZain@users.noreply.github.com>
2021-03-20 21:22:22 +0530

    done with apis

--- client/src/pages/Exercise/index.js
+++ client/src/pages/Exercise/index.js
@@ -1,37 +1,68 @@
 import React, { useState } from 'react'
 import styles from './index.module.css'
-import { Form, Row, Col ,Button} from 'react-bootstrap'
+import { Form, Row, Col, Button, Toast } from 'react-bootstrap'
 import axios from 'axios'
 import Autocomplete from 'react-autocomplete'
 const Exercise = () => {
-    const [meal,setMeal] = useState("")
-    const [ex,setEx] = useState("")
-    const [disp,setDisp] = useState("")
-    const  [formdata, setFormdata] = useState("")
+    const [meal, setMeal] = useState("")
+    const [ex, setEx] = useState("")
+    const [disp, setDisp] = useState("")
+    const [calGained, setCalG] = useState(null)
+    const [calLost, setCalL] = useState(null)
+    const [formdata, setFormdata] = useState("")
 
     const fetchMeal = async () => {
 
-        await axios.post(`https://trackapi.nutritionix.com/v2/natural/nutrients`, {
+        const params = new URLSearchParams()
+        params.append('query', meal)
+        await axios.post(`https://trackapi.nutritionix.com/v2/natural/nutrients`, params, {
             headers: {
                 "x-app-id": process.env.REACT_APP_API_ID,
                 "x-app-key": process.env.REACT_APP_API_KEY,
-                "x-remote-user-id": process.env.REACT_APP_REMOTE_ID
+                "x-remote-user-id": process.env.REACT_APP_REMOTE_ID,
+                'Content-Type': 'application/x-www-form-urlencoded'
             }
         })
             .then(res => {
-                const temp_data = res.data.common.map((val, ind) => {
-                   console.log(temp_data)
-                })
-                setData(temp_data)
+                const temp_data = res.data.foods.map((val, ind) => {
+                    return val.nf_calories
+                }).reduce((el, ac) => (el + ac))
+                console.log(temp_data)
+                setCalG(temp_data)
+
             }).catch(err => {
                 console.log(err)
             })
 
     }
-    const submit = (e) => {
+    const fetchEx = async () => {
+        const params = new URLSearchParams()
+        params.append('query', 'squats')
+        await axios.post(`https://trackapi.nutritionix.com/v2/natural/exercise`, params, {
+            headers: {
+                "x-app-id": process.env.REACT_APP_API_ID,
+                "x-app-key": process.env.REACT_APP_API_KEY,
+                "x-remote-user-id": process.env.REACT_APP_REMOTE_ID,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+            .then(res => {
+                const temp_data = res.data.exercises.map((val, ind) => {
+                    return val.nf_calories
+                }).reduce((el, ac) => (el + ac))
+                console.log(temp_data)
+                setCalL(temp_data)
+            }).catch(err => {
+                console.log(err)
+            })
+    }
+
+
+    const submit = async (e) => {
         console.log("worked")
-        fetchMeal();
-            
+        await fetchMeal();
+        await fetchEx();
+
     }
     const ipHandler1 = (e) => {
         setMeal(e.target.value)
@@ -46,7 +77,7 @@ const Exercise = () => {
         { label: 'pear' }
     ])
     console.log(search)
-   // console.log(process.env.REACT_APP_API_ID)
+    // console.log(process.env.REACT_APP_API_ID)
 
 
     const handleUpdate = async (e, results) => {
@@ -61,11 +92,7 @@ const Exercise = () => {
             }
         })
             .then(res => {
-                const temp_data = res.data.common.map((val, ind) => {
-                    return {
-                        label: val.food_name
-                    }
-                })
+                const temp_data = null;
                 setData(temp_data)
             }).catch(err => {
                 console.log(err)
@@ -75,9 +102,10 @@ const Exercise = () => {
     return (
         <div className={styles.container}>
             <h2>Current Score : </h2>
-            <Form >
-                <Form.Group as={Row} controlId="formHorizontalEmail">
-                    {/* <Form.Label column sm={2}>
+            <Form className="container" >
+
+                {/* <Form.Group as={Row} controlId="formHorizontalEmail">
+                    <Form.Label column sm={2}>
                     Food
                     </Form.Label>
                     <Col sm={10}>
@@ -94,28 +122,62 @@ const Exercise = () => {
                         onChange={handleUpdate}
                         onSelect={(val) => setSearch(val)}
                     />
-                    </Col> */}
+                    </Col> 
 
                     <Form.Label column sm={2}>
                         Exercise
                     </Form.Label>
                     <Col sm="10">
                         <Form.Control type="textarea" placeholder="1000 steps" />
-                    </Col>
-                </Form.Group>
+                    </Col> 
+             </Form.Group>  */}
 
                 <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>So What do you had for your Meal ?? </Form.Label>
-                    <Form.Control onChange = {ipHandler1} as="textarea" rows={3} onClick = {submit} />
+                    <Form.Label  >So What do you had for your Meal ?? </Form.Label>
+                    <Row>
+                        <Col><Form.Control onChange={ipHandler1} as="textarea" rows={3} /></Col>
+
+                    </Row>
+                    <Row>
+
+                        <Col>
+                            <h4>Calories Gained</h4>
+                            {calGained ? calGained : 0}
+
+                        </Col>
+                    </Row>
+
                 </Form.Group>
 
                 <Form.Group controlId="exampleForm.ControlTextarea2">
                     <Form.Label>Tell Me About Your workout session??</Form.Label>
-                    <Form.Control onChange = {ipHandler2} as="textarea" rows={3} />
+                    <Row>
+                        <Col><Form.Control onChange={ipHandler2} as="textarea" rows={3} /></Col>
+
+                    </Row>
+                    <Row>
+
+                        <Col>
+                            <h4>Calories Burned</h4>
+                            {calLost ? calLost : 0}
+
+                        </Col>
+                    </Row>
+
                 </Form.Group>
-                <Button onClick = {submit}  variant="primary">Submit</Button>{' '}
+                <Button onClick={submit} variant="primary">Submit</Button>{' '}
+
             </Form>
-            
+            <Toast>
+                <Toast.Header>
+                    <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
+                    <strong className="mr-auto">Bootstrap</strong>
+                    <small>11 mins ago</small>
+                </Toast.Header>
+                <Toast.Body>Hello, world! This is a toast message.</Toast.Body>
+            </Toast>
+
+
         </div>
     )
 }
