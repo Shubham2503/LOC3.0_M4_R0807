e6f39489687317a34e8418746bce10584c2849c0
Harsh Kumar <44499896+Harshkumar10@users.noreply.github.com>
2021-03-21 01:11:52 +0530

    temp changes

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,7 @@ const User = require("../models/user");
 
 const auth = async (req, res, next) => {
     try {
-        console.log("fsaf");
+        // console.dir(req.header);
         const token = req.header("Authorization").replace("Bearer ", "");
         console.log(token);
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
