ee58e3ae6298530c805300a47fc475203e9b132a
Shubham <Shubhamp7188@gmail.com>
2021-03-20 19:08:00 +0530

    Added create post page

--- client/src/pages/Createpost/index.js
+++ client/src/pages/Createpost/index.js
@@ -1,10 +1,41 @@
 import React from 'react'
+import styles from './index.module.css'
+import { Form, Button, Col, Row } from 'react-bootstrap'
 
 
 const Createpost = () => {
-    return(
-        <div>
-            <h1>cavbcj</h1>
+    return (
+        <div className={styles.container}>
+            <Form>
+                <Form.Row>
+                    <Form.Group as={Col}>
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+                </Form.Row>
+
+                <Form.Group>
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control as="textarea" placeholder="Add Description" />
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label>Image</Form.Label>
+                    <Form.Control  placeholder="Image URL" />
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.File
+                    className="position-relative"
+                    name="file"
+                    label="File"
+                    />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     )
 }
