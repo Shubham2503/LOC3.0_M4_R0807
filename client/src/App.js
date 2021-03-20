a55c20d2cc23b70fc33c896cacf704ab8b6da278
Shubham <Shubhamp7188@gmail.com>
2021-03-20 23:14:34 +0530

    added login initial page

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './App.module.css'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Home from './pages/Home'
 import Scoreboard from './pages/Scoreboard'
 import Exercise from './pages/Exercise'
 import Createpost from './pages/Createpost'
+import Login from './pages/Login'
 import Post_tag from './pages/Post_tag'
 import Navbar from './components/Navbar'
 
 const App = () => {
+
+    const [isLogedin, setIsLogedin] = useState(false)
+    if(!isLogedin)
+        return (
+            <Login />
+        )
+
     return (
+
         <div className={styles.container}>
             <Router>
                 <Navbar />
---
+++ client/src/pages/Login/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+    return(
+        <div>
+            Log
+        </div>
+    )
+}
+
+export default Login;
