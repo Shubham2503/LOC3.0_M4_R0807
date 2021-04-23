7eceece1c125ab3703193e8aad41c384c8fe508a
Ismail <ismailparsola@gmail.com>
2021-04-23 14:02:50 +0530

    login modified

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -9,124 +9,89 @@ import MuiAlert from "@material-ui/lab/Alert";
 import { makeStyles } from "@material-ui/core/styles";
 
 function Alert(props) {
-    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 const useStyles = makeStyles((theme) => ({
-    root: {
-        width: "100%",
-        "& > * + *": {
-            marginTop: theme.spacing(2),
-        },
+  root: {
+    width: "100%",
+    "& > * + *": {
+      marginTop: theme.spacing(2),
     },
+  },
 }));
 const Login = (props) => {
-    const classes = useStyles();
-    const [open, setOpen] = React.useState(false);
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
 
-    const handleClick = () => {
-        setOpen(true);
-    };
+  const handleClick = () => {
+    setOpen(true);
+  };
 
-    const handleClose = (event, reason) => {
-        if (reason === "clickaway") {
-            return;
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+  const [success, setSuccess] = useState(false);
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [error, setError] = useState(false);
+
+  const inp1 = (e) => {
+    setEmail(e.target.value);
+  };
+  const inp2 = (e) => {
+    setPass(e.target.value);
+  };
+
+  const is_loaded = (val) => {
+    props.is_loaded(val);
+  };
+  const submit = async (e) => {
+    e.preventDefault();
+    await axios
+      .post(`/user/login`, {
+        email: email,
+        password: pass,
+      })
+      .then((res) => {
+        const temp_data = res.data;
+        console.log(temp_data);
+        if (temp_data) {
+          Cookies.set("id", temp_data);
+
+          is_loaded(true);
+          setError(false);
+          console.log(error);
         }
+      })
+      .catch((err) => {
+        setError(true);
+        console.log(err);
+        setSuccess(false);
+        setOpen(true);
+      });
+  };
 
-        setOpen(false);
-    };
-    const [success, setSuccess] = useState(false);
-    const [email, setEmail] = useState("");
-    const [pass, setPass] = useState("");
-    const [error, setError] = useState(false);
+  return (
+    <>
+      <SignInSide
+        onEmailChange={inp1}
+        onPasswordChange={inp2}
+        onSubmit={submit}
+      />
 
-    const inp1 = (e) => {
-        setEmail(e.target.value);
-    };
-    const inp2 = (e) => {
-        setPass(e.target.value);
-    };
-
-    const is_loaded = (val) => {
-        props.is_loaded(val);
-    };
-    const submit = async () => {
-        await axios
-            .post(`/user/login`, {
-                email: email,
-                password: pass,
-            })
-            .then((res) => {
-                const temp_data = res.data;
-                console.log(temp_data);
-                if (temp_data) {
-                    Cookies.set("id", temp_data);
-
-                    is_loaded(true);
-                    setError(false);
-                    console.log(error);
-                }
-            })
-            .catch((err) => {
-                setError(true);
-                console.log(err);
-                setSuccess(false);
-                setOpen(true);
-            });
-    };
-
-    return (
-        <>
-            <SignInSide
-                onEmailChange={inp1}
-                onPasswordChange={inp2}
-                onSubmit={submit}
-            />
-            <div className={styles.box}>
-                <h3>Log in</h3>
-
-                <div className="form-group">
-                    <label>Email</label>
-                    <input
-                        onChange={inp1}
-                        value={email}
-                        name="email"
-                        type="email"
-                        className="form-control"
-                        placeholder="Enter email"
-                    />
-                </div>
-
-                <div className="form-group">
-                    <label>Password</label>
-                    <input
-                        onChange={inp2}
-                        value={pass}
-                        name="password"
-                        type="password"
-                        className="form-control"
-                        placeholder="Enter password"
-                    />
-                </div>
-                <button
-                    onClick={submit}
-                    className="btn btn-dark btn-lg btn-block"
-                >
-                    Sign in
-                </button>
-            </div>
-            <div className={classes.root}>
-                <Snackbar
-                    open={open}
-                    autoHideDuration={6000}
-                    onClose={handleClose}
-                >
-                    <Alert onClose={handleClose} severity="error">
-                        Check your email id and password!
-                    </Alert>
-                </Snackbar>
-            </div>
-        </>
-    );
+      <div className={classes.root}>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="error">
+            Incorrect email or password!
+          </Alert>
+        </Snackbar>
+      </div>
+    </>
+  );
 };
 
 export default Login;
