9f574b8cfeceb57a1ed769a93ccedcef403731fe
Harsh Kumar <44499896+Harshkumar10@users.noreply.github.com>
2021-04-30 15:15:15 +0530

    added home

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,15 +3,15 @@ import Cookies from "js-cookie";
 import { useHistory } from "react-router";
 
 import {
-  Button,
-  Divider,
-  Drawer,
-  IconButton,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  makeStyles,
+    Button,
+    Divider,
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    makeStyles,
 } from "@material-ui/core";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -21,107 +21,122 @@ import MailIcon from "@material-ui/icons/Mail";
 import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-  list: {
-    width: 250,
-  },
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+    list: {
+        width: 250,
+    },
 }));
 
 const Navbr = (props) => {
-  const classes = useStyles();
-  const history = useHistory();
-  const [drawerOpen, setDrawerOpen] = useState(false);
+    const classes = useStyles();
+    const history = useHistory();
+    const [drawerOpen, setDrawerOpen] = useState(false);
 
-  const logout = () => {
-    props.is_loaded(false);
-    Cookies.remove("id");
-  };
+    const logout = () => {
+        props.is_loaded(false);
+        Cookies.remove("id");
+    };
+    const home = () => {
+        history.push("/");
+    };
 
-  return (
-    <div>
-      <AppBar color="primary" position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon
-              onClick={() => {
-                setDrawerOpen(true);
-              }}
-            />
-          </IconButton>
-          <Typography
-            variant="h5"
-            className={classes.title}
-            onClick={() => {
-              history.push(`/`);
-            }}
-          >
-            Fittfy
-          </Typography>
-          <Button onClick={logout} color="inherit">
-            Logout
-          </Button>
-        </Toolbar>
-      </AppBar>
+    return (
+        <div>
+            <AppBar color="primary" position="static">
+                <Toolbar>
+                    <IconButton
+                        edge="start"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="menu"
+                    >
+                        <MenuIcon
+                            onClick={() => {
+                                setDrawerOpen(true);
+                            }}
+                        />
+                    </IconButton>
+                    <Typography
+                        variant="h5"
+                        className={classes.title}
+                        onClick={() => {
+                            history.push(`/`);
+                        }}
+                    >
+                        Fittfy
+                    </Typography>
+                    <Button onClick={logout} color="inherit">
+                        Logout
+                    </Button>
+                    <Button onClick={home} color="inherit">
+                        Home
+                    </Button>
+                </Toolbar>
+            </AppBar>
 
-      <Drawer
-        className={"hello"}
-        anchor="left"
-        open={drawerOpen}
-        onClose={() => {
-          setDrawerOpen(!drawerOpen);
-        }}
-      >
-        <Divider />
-        <List style={{ width: "250px" }}>
-          {[
-            "Home",
-            "Scoreboard",
-            "Exercise",
-            "Goal",
-            "Create Post",
-            "User",
-          ].map((text, index) => (
-            <div
-              role="presentation"
-              onClick={() => {
-                setDrawerOpen(false);
-              }}
-              onKeyDown={() => {
-                setDrawerOpen(false);
-              }}
-            >
-              <ListItem
-                button
-                key={text}
-                onClick={() => {
-                  history.push(`/${text.toLowerCase().split(" ").join("")}`);
+            <Drawer
+                className={"hello"}
+                anchor="left"
+                open={drawerOpen}
+                onClose={() => {
+                    setDrawerOpen(!drawerOpen);
                 }}
-              >
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            </div>
-          ))}
-        </List>
-      </Drawer>
-      <hr />
-    </div>
-  );
+            >
+                <Divider />
+                <List style={{ width: "250px" }}>
+                    {[
+                        "Home",
+                        "Scoreboard",
+                        "Exercise",
+                        "Goal",
+                        "Create Post",
+                        "User",
+                    ].map((text, index) => (
+                        <div
+                            role="presentation"
+                            onClick={() => {
+                                setDrawerOpen(false);
+                            }}
+                            onKeyDown={() => {
+                                setDrawerOpen(false);
+                            }}
+                        >
+                            <ListItem
+                                button
+                                key={text}
+                                onClick={() => {
+                                    history.push(
+                                        `/${text
+                                            .toLowerCase()
+                                            .split(" ")
+                                            .join("")}`
+                                    );
+                                }}
+                            >
+                                <ListItemIcon>
+                                    {index % 2 === 0 ? (
+                                        <InboxIcon />
+                                    ) : (
+                                        <MailIcon />
+                                    )}
+                                </ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItem>
+                        </div>
+                    ))}
+                </List>
+            </Drawer>
+            <hr />
+        </div>
+    );
 };
 
 export default Navbr;
